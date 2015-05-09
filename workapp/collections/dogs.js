ca73be67e6be1b812e59b1be4a4ea4784e21b398
Sean Niesen <sniesen@nectarine.unival.com>
2015-05-08 17:01:20 -0700

    going home

--- workapp/collections/dogs.js
+++ workapp/collections/dogs.js
@@ -2,4 +2,18 @@ Dogs = new Meteor.Collection('dogs', {
 	transform: function(dogs) {
 		return dogs;
 	}
-})
+});
+
+Dogs.attachSchema(new SimpleSchema({
+	name: {
+		type: String,
+		label: "Name",
+		max: 20,
+		optional: false
+	},
+	birthday: {
+		type: Date,
+		label: "Birthday",
+		optional: true
+	}
+}));
